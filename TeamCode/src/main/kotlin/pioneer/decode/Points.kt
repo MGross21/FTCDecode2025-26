abc4b4cec22447581aae6e02cc09bf72f78e164e
MGross21 <mgstinger521@gmail.com>
2025-12-20 12:26:04 -0700

    Refactor Points class to streamline pose transformations and add PARK_BASE position

--- TeamCode/src/main/kotlin/pioneer/decode/Points.kt
+++ TeamCode/src/main/kotlin/pioneer/decode/Points.kt
@@ -24,35 +24,41 @@ BLUE ALLIANCE   |               0----> +X       |    RED ALLIANCE
 */
 
 // All points are defined from the RED ALLIANCE perspective
+// Desmos Link: https://www.desmos.com/calculator/ojosiylesc
 class Points(
-    color: AllianceColor,
+    private val color: AllianceColor,
 ) {
-    // Function to transform a point based on alliance color
-    fun Pose.T(c: AllianceColor): Pose =
-        when (c) {
+    private fun Pose.transform(): Pose =
+        when (color) {
             AllianceColor.RED -> this
             AllianceColor.BLUE -> Pose(-this.x, this.y, -this.theta)
             AllianceColor.NEUTRAL -> this
         }
 
+    private fun fieldPose(x: Double, y: Double, theta: Double): Pose =
+        Pose(x = x, y = y, theta = theta).transform()
+
     // Key positions on the field
     // Written in ACTION_POSITION format
-    val START_GOAL = Pose(130.0, 137.0, theta = 3 * PI / 4).T(color)
-    val START_FAR = Pose(43.0, -157.0, theta = 0.0).T(color)
+    val START_GOAL: Pose = fieldPose(130.0, 137.0, 3 * PI / 4)
+    val START_FAR: Pose = fieldPose(43.0, -157.0, 0.0)
 
-    val SHOOT_GOAL_CLOSE = Pose(60.0, 60.0, theta = 0.0).T(color)
-    val SHOOT_GOAL_FAR = Pose(43.0, -145.0, theta = 0.0).T(color)
-
+    val SHOOT_GOAL_CLOSE: Pose = fieldPose(60.0, 60.0, 0.0)
+    val SHOOT_GOAL_FAR: Pose = fieldPose(43.0, -145.0, 0.0)
     private fun collectY(i: Int) = 30 - (i * 60.0) // 30, -30, -90
-
+    
+    // Collection
     private val prepCollectX = 70.0
-    private val collectTheta = -PI / 2 // Point to right
-    val PREP_COLLECT_GOAL = Pose(prepCollectX, collectY(0), collectTheta).T(color)
-    val PREP_COLLECT_MID = Pose(prepCollectX, collectY(1), collectTheta).T(color)
-    val PREP_COLLECT_AUDIENCE = Pose(prepCollectX, collectY(2), collectTheta).T(color)
-
     private val collectX = 130.0
-    val COLLECT_GOAL = Pose(collectX, collectY(0), collectTheta).T(color)
-    val COLLECT_MID = Pose(collectX, collectY(1), collectTheta).T(color)
-    val COLLECT_AUDIENCE = Pose(collectX, collectY(2), collectTheta).T(color)
+    private val collectTheta = -PI / 2 // Point to right
+
+    val PREP_COLLECT_GOAL: Pose = fieldPose(prepCollectX, collectY(0), collectTheta)
+    val PREP_COLLECT_MID: Pose = fieldPose(prepCollectX, collectY(1), collectTheta)
+    val PREP_COLLECT_AUDIENCE: Pose = fieldPose(prepCollectX, collectY(2), collectTheta)
+    
+    val COLLECT_GOAL: Pose = fieldPose(collectX, collectY(0), collectTheta)
+    val COLLECT_MID: Pose = fieldPose(collectX, collectY(1), collectTheta)
+    val COLLECT_AUDIENCE: Pose = fieldPose(collectX, collectY(2), collectTheta)
+
+    val PARK_BASE: Pose = fieldPose(85.0, -100.0, -PI / 2)
 }
